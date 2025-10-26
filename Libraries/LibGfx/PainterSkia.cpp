82d8367b43727ac202237a25bd80a4b0851a4516
Tim Ledbetter <tim.ledbetter@ladybird.org>
2025-10-26 19:06:13 +0000

    LibWeb: Implement `CanvasPattern.setTransform()`
    This method applies the given transformation matrix to a pattern.

--- Libraries/LibGfx/PainterSkia.cpp
+++ Libraries/LibGfx/PainterSkia.cpp
@@ -101,14 +101,23 @@ static void apply_paint_style(SkPaint& paint, PaintStyle const& style)
         auto repeat_x = first_is_one_of(repetition, CanvasPatternPaintStyle::Repetition::Repeat, CanvasPatternPaintStyle::Repetition::RepeatX);
         auto repeat_y = first_is_one_of(repetition, CanvasPatternPaintStyle::Repetition::Repeat, CanvasPatternPaintStyle::Repetition::RepeatY);
 
-        // FIXME: Implement sampling configuration.
         SkSamplingOptions sk_sampling_options { SkFilterMode::kLinear };
+        Optional<SkMatrix> transformation_matrix;
 
+        if (canvas_pattern->transform().has_value()) {
+            auto const& transform = canvas_pattern->transform().value();
+            transformation_matrix = SkMatrix::MakeAll(
+                transform.a(), transform.c(), transform.e(),
+                transform.b(), transform.d(), transform.f(),
+                0, 0, 1);
+        }
         auto shader = sk_image->makeShader(
             repeat_x ? SkTileMode::kRepeat : SkTileMode::kDecal,
             repeat_y ? SkTileMode::kRepeat : SkTileMode::kDecal,
-            sk_sampling_options);
-        paint.setShader(shader);
+            sk_sampling_options, transformation_matrix.has_value() ? &transformation_matrix.value() : nullptr);
+
+        paint.setAntiAlias(false);
+        paint.setShader(move(shader));
     } else {
         dbgln("FIXME: Unsupported PaintStyle");
     }
