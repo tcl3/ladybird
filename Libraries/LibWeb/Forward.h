8d4f8a2d7fffb0fdf27cfdfd7165a2e275db9eec
Tim Ledbetter <tim.ledbetter@ladybird.org>
2026-04-17 12:04:56 +0100

    LibWeb: Route lch()/oklch() through unified `ColorFunctionStyleValue`

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -362,7 +362,6 @@ class MediaQueryListEvent;
 class Number;
 class NumberStyleValue;
 class NumericType;
-class OKLCHColorStyleValue;
 class OpenTypeTaggedStyleValue;
 class ParsedFontFace;
 class PendingSubstitutionStyleValue;
